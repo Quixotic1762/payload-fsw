da9932992685278f53fb41d7e81fc4b2ae8fe14a
Ashwin Kumar <gunnerash0005@gmail.com>
2025-08-23 03:06:49 +0530

    added gsm script to fsw

--- fsw.py
+++ fsw.py
@@ -350,6 +350,30 @@ def lora_proc():
     main()
 
 def gsm_proc():
+    import serial
+import time
+
+ser = serial.Serial("/dev/ttyAMA2", 9600, timeout=1)
+
+def send_at(command):
+    tr_buffer = (command+'\r\n').encode(encoding="utf-8")
+    ser.write((command+'\r\n').encode())
+    while ser.readline():
+        print(ser.readline().decode('utf-8'))
+
+        
+def send_sms(number, message):
+    send_at("AT+CMGF=1")
+    set_nu = 'AT+CMGS="'+number+'"'
+    send_at(set_nu)
+    msg = message + chr(26)
+    msg_bytes = msg.encode()
+    ser.write(msg_bytes)
+    
+
+while True:
+    send_sms('+918984955170', 'Helo from rpi and sim800')
+    time.sleep(1)
 
 def state_machine_proc():
 
